16904a038b1b198a6c718b849e7e491303eefe47
Chones17 <chones17@byui.edu>
2023-01-07 14:21:57 -0700

    Create server.js
    Create Express object, handle http requests, and set port listener

---
+++ server.js
@@ -0,0 +1,12 @@
+// Create the Express object and set port
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Handles any http requests
+app.use('/', require('./routes'));
+
+// Listens to port and logs event to the console
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
