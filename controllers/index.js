acadfca4899f66a5d2c24f118d099393c3f0b37a
Chones17 <chones17@byui.edu>
2023-01-07 17:23:15 -0700

    Add Controllers
    Add controllers to API

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+// Controller function for routes
+const controller = (req, res) => {
+
+    // Send Reponse to http GET requests
+    res.send('Travis Stirling');
+};
+
+// Export controller function
+module.exports = controller;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,7 @@
 const routes = require('express').Router();
 
 // Handles any http GET requests
-routes.get('/', (req, res) => {
-
-    // Send Reponse to http GET requests
-    res.send('Travis Stirling');
-});
+routes.get('/', require('../controllers'));
 
 // Export routes object
 module.exports = routes;
