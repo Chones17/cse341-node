7245cfcd45412ab5322065b267bcd9ed121624c4
Chones17 <chones17@byui.edu>
2023-01-14 09:03:09 -0700

    Create index.js
    Add Backend controller

---
+++ backend/controller/index.js
@@ -0,0 +1,9 @@
+// Controller function for routes
+const controller = (req, res) => {
+    
+    // Send Reponse to http GET requests
+    res.send('This is a test');
+};
+
+// Export controller function
+module.exports = controller;
