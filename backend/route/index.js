7061b0abc71efa791a28396f2f5c8129d75e87f7
Chones17 <chones17@byui.edu>
2023-01-14 09:06:57 -0700

    Create index.js
    Add router to Backend application

---
+++ backend/route/index.js
@@ -0,0 +1,8 @@
+// Create the router object
+const route = require('express').Router();
+
+// Handles any http GET requests
+route.get('/', require('../controller'));
+
+// Export routes object
+module.exports = route;
