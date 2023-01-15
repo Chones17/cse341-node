7b4de91aea8a49e4c4120f96909dfc0da29d68d2
Chones17 <chones17@byui.edu>
2023-01-14 17:54:03 -0700

    Update server.js
    Simplified the main js file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Handles any http requests
-app.use('/', require('./routes'));
+app.use('/', require('./route'));
 
 // Listens to port and logs event to the console
 app.listen(port, () => {
