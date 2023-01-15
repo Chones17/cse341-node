d3c0248ba7bfc31d496dfc21485ce987db83e9dc
Chones17 <chones17@byui.edu>
2023-01-14 17:54:27 -0700

    Create index.js
    Adjust naming convention for Controller directory

---
+++ controller/index.js
@@ -0,0 +1,9 @@
+// Controller function for routes
+const controller = async (req, res) => {
+
+    // Send Reponse to http GET requests
+    res.send('Home');
+};
+
+// Export controller function
+module.exports = controller;
