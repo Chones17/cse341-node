c1f744452e655cc633fb2ed5654cc796c791f206
Chones17 <chones17@byui.edu>
2023-01-14 17:53:06 -0700

    Delete index.js
    Adjusting naming conventions for Controller directory

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-// Controller function for routes
-const controller = (req, res) => {
-
-    // Send Reponse to http GET requests
-    res.send('Travis Stirling');
-};
-
-// Export controller function
-module.exports = controller;
