33baa009cd569d3c6266a9e2d0c1c2aff7d10d17
Chones17 <chones17@byui.edu>
2023-01-21 16:10:58 -0700

    Update contacts.js
    Add handlers for POST, PUT, and DELETE requests for contacts

--- route/contacts.js
+++ route/contacts.js
@@ -8,5 +8,14 @@ const contactsController = require('../controller/contacts');
 route.get('/', contactsController.getContacts);
 route.get('/:id', contactsController.getOne);
 
+// Handles any http POST requests
+route.post('/', contactsController.createContact);
+
+// Handles any http PUT requests
+route.put('/:id', contactsController.updateContact);
+
+// Handles any http DELETE requests
+route.delete('/:id', contactsController.deleteContact);
+
 // Export routes object
 module.exports = route;
