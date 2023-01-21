44163a5a99eced1fe83018dfa88ef1dc8ca08fa8
Chones17 <chones17@byui.edu>
2023-01-21 16:07:33 -0700

    Update server.js
    Add body-parser to parse the the body of the http request

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // Create the Express object and set port
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+
+// Decode the body from the HTTP request
+app.use(bodyParser.json());
 
 // Handles any http requests
 app.use('/', require('./route'));
