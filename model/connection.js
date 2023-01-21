41e98317d446be76f894ee37f6790b23a5081925
Chones17 <chones17@byui.edu>
2023-01-21 16:15:22 -0700

    Update connection.js
    Add database connections for POST, PUT, and DELETE requests

--- model/connection.js
+++ model/connection.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
 // Database connection handler
-const contacts = async () => {
+const dbGetContacts = async () => {
 
     // Connection URI imported from dotenv
     const uri = process.env.MONGODB_URI;
@@ -16,7 +16,7 @@ const contacts = async () => {
 
         // Connect to the MongoDB cluster
         const database = client.db('test');
-        const collection = database.collection('contacts');      
+        const collection = database.collection('contacts');
         const contacts = await collection.find().toArray();
     
         console.log('Database connection started');
@@ -38,7 +38,7 @@ const contacts = async () => {
 }
 
 // Database connection handler
-const oneContact = async (userId) => {
+const dbOneContact = async (userId) => {
 
     // Connection URI imported from dotenv
     const uri = process.env.MONGODB_URI;
@@ -52,7 +52,7 @@ const oneContact = async (userId) => {
         const user = new ObjectId(userId);
         const database = client.db('test');
         const collection = database.collection('contacts');      
-        const contact = await collection.find({_id: user}).toArray();
+        const contact = await collection.findOne({_id: user});
     
         console.log('Database connection started');
 
@@ -72,4 +72,113 @@ const oneContact = async (userId) => {
     }
 }
 
-module.exports = { contacts, oneContact };
+// Database connection handler
+const dbCreateContact = async (contact) => {
+
+    // Connection URI imported from dotenv
+    const uri = process.env.MONGODB_URI;
+
+    // Create client to interact with MongoDB
+    const client = new MongoClient(uri);
+
+    try {
+
+        // Connect to the MongoDB cluster
+        const database = client.db('test');
+        const collection = database.collection('contacts');
+        const contacts = await collection.insertOne(contact);
+    
+        console.log('Database connection started');
+
+        return contacts;
+
+    } catch (e) {
+
+        // Log the error to the console
+        console.error(e);
+
+    } finally {
+
+        // Close connection to the MongoDB cluster
+        await client.close();
+
+        console.log('Database connection stopped')
+    }
+}
+
+// Database connection handler
+const dbUpdateContact = async (userId, updateContact) => {
+
+    // Connection URI imported from dotenv
+    const uri = process.env.MONGODB_URI;
+
+    // Create client to interact with MongoDB
+    const client = new MongoClient(uri);
+
+    try {
+
+        // Connect to the MongoDB cluster
+        const user = new ObjectId(userId);
+        const database = client.db('test');
+        const collection = database.collection('contacts');      
+        const contact = await collection.replaceOne({_id: user}, updateContact);
+    
+        console.log('Database connection started');
+
+        return contact;
+
+    } catch (e) {
+
+        // Log the error to the console
+        console.error(e);
+
+    } finally {
+
+        // Close connection to the MongoDB cluster
+        await client.close();
+
+        console.log('Database connection stopped')
+    }
+}
+
+const dbDeleteContact = async (userId) => {
+
+    // Connection URI imported from dotenv
+    const uri = process.env.MONGODB_URI;
+
+    // Create client to interact with MongoDB
+    const client = new MongoClient(uri);
+
+    try {
+
+        // Connect to the MongoDB cluster
+        const user = new ObjectId(userId);
+        const database = client.db('test');
+        const collection = database.collection('contacts');      
+        const contact = await collection.deleteOne({_id: user});
+    
+        console.log('Database connection started');
+
+        return contact;
+
+    } catch (e) {
+
+        // Log the error to the console
+        console.error(e);
+
+    } finally {
+
+        // Close connection to the MongoDB cluster
+        await client.close();
+
+        console.log('Database connection stopped')
+    }
+}
+
+module.exports = { 
+    dbGetContacts, 
+    dbOneContact, 
+    dbCreateContact, 
+    dbUpdateContact, 
+    dbDeleteContact 
+};
