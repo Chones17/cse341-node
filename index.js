03d1e32ea3d36c628c903b4f250b1932aaa20237
Chones17 <chones17@byui.edu>
2023-02-04 16:31:16 -0700

    Update index.js
    Add Date logic to the log and update routes path.

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
+// Create Date object to track listening
+let date = new Date();
+
 // Use CORS, JSON, UrlEncoded, and route for application
 app
     .use(cors({origin: '*'}))
     .use(express.json())
     .use(express.urlencoded({extended: true}))
-    .use('/', require('./route'));
+    .use('/', require('./routes'));
 
 // Listens to port and logs event to the console
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}: ${date}`);
 });
