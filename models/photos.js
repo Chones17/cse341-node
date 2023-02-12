e13dd584ef39bd5604fa1f85f6a5dd1b59edbeec
Chones17 <chones17@byui.edu>
2023-02-11 21:54:20 -0700

    Update photos.js
    Add PUT and DELETE database connection handlers

--- models/photos.js
+++ models/photos.js
@@ -124,5 +124,93 @@ const dbPostPhoto = async (photoId) => {
     }
 }
 
+// Database connection handler
+const dbPutPhoto = async (photoId, updatePhoto) => {
+
+    // Connection URI imported from dotenv
+    const uri = process.env.MONGODB_URI;
+
+    // Create client to interact with MongoDB
+    const client = new MongoClient(uri);
+
+    try {
+
+        // Create Date object to track database connection
+        let date = new Date();
+
+        // Connect to the MongoDB cluster
+        const id = new ObjectId(photoId);
+        const database = client.db('spencer');
+        const collection = database.collection('photos');      
+        const photo = await collection.replaceOne({_id: id}, updatePhoto);
+    
+        console.log(`Database connection started: ${date}`);
+
+        return photo;
+
+    } catch (e) {
+
+        // Log the error to the console
+        console.error(e);
+
+    } finally {
+
+        // Create Date object to track database connection
+        let date = new Date();
+
+        // Close connection to the MongoDB cluster
+        await client.close();
+
+        console.log(`Database connection stopped: ${date}`);
+    }
+}
+
+// Database connection handler
+const dbDeletePhoto = async (photoId) => {
+
+    // Connection URI imported from dotenv
+    const uri = process.env.MONGODB_URI;
+
+    // Create client to interact with MongoDB
+    const client = new MongoClient(uri);
+
+    try {
+
+        // Create Date object to track database connection
+        let date = new Date();
+
+        // Connect to the MongoDB cluster
+        const id = new ObjectId(photoId);
+        const database = client.db('spencer');
+        const collection = database.collection('photos');      
+        const photo = await collection.deleteOne({_id: id});
+    
+        console.log(`Database connection started: ${date}`);
+
+        return photo;
+
+    } catch (e) {
+
+        // Log the error to the console
+        console.error(e);
+
+    } finally {
+
+        // Create Date object to track database connection
+        let date = new Date();
+
+        // Close connection to the MongoDB cluster
+        await client.close();
+
+        console.log(`Database connection stopped: ${date}`);
+    }
+}
+
 // Export Photos model functions
-module.exports = { dbGetPhotos, dbGetPhoto, dbPostPhoto };
+module.exports = { 
+    dbGetPhotos, 
+    dbGetPhoto, 
+    dbPostPhoto, 
+    dbPutPhoto, 
+    dbDeletePhoto 
+};
