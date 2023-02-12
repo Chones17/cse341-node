5a81658c24a2da00af0327f97fdc1bf50dcfa57d
Chones17 <chones17@byui.edu>
2023-02-11 21:53:40 -0700

    Update photos.js
    Add PUT and DELETE controllers

--- controllers/photos.js
+++ controllers/photos.js
@@ -1,5 +1,10 @@
 // Require the Photo database connections
-const { dbGetPhotos, dbGetPhoto, dbPostPhoto } = require('../models/photos');
+const { 
+    dbGetPhotos, 
+    dbGetPhoto, 
+    dbPostPhoto, 
+    dbPutPhoto, 
+    dbDeletePhoto } = require('../models/photos');
 
 // Controller function to retrieve all photos
 const getPhotos = async (req, res) => {
@@ -15,7 +20,7 @@ const getPhotos = async (req, res) => {
 // Controller function to retrieve one photo
 const getPhoto = async (req, res) => {
 
-    await dbGetPhoto(req.params['id']).then(results => {
+    await dbGetPhoto(req.params.id).then(results => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200);
         res.json(results);
@@ -43,9 +48,48 @@ const postPhoto = async (req, res) => {
         res.json(response);
     } else {
         res.status(500);
-        res.json(response.error || 'An error occurred while creating the contact.');
+        res.json(response.error || 'An error occurred while creating the document.');
+    }
+};
+
+// Controller function to update photo
+const putPhoto = async (req, res) => {
+
+    // Set the request body
+    const photo = {
+        title: req.body.title,
+        photographer: req.body.photographer,
+        description: req.body.description,
+        location: req.body.location,
+        publishDate: req.body.publishDate,
+        fileName: req.body.fileName,
+        filePath: req.body.filePath
+    };
+
+    const response = await dbPutPhoto(req.params.id, photo);
+  
+    if (response.modifiedCount > 0) {
+        res.status(204);
+        res.send();
+    } else {
+        res.status(500);
+        res.json(response.error || 'An error occurred while updating the document.');
+    }
+};
+
+// Controller function to delete photo
+const deletePhoto = async (req, res) => {
+
+    const response = await dbDeletePhoto(req.params['id']);
+
+    if (response.deletedCount > 0) {
+        res.status(200);
+        res.send();
+    } else {
+        res.status(500);
+        res.json(response.error || 'An error occurred while deleting the document.');
     }
 };
 
 // Export Photos controller functions
-module.exports = { getPhotos, getPhoto, postPhoto };
+module.exports = { getPhotos, getPhoto, postPhoto, putPhoto, deletePhoto };
