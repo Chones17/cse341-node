8a4699a9947396495fc10eaa9488651c4b423527
Chones17 <chones17@byui.edu>
2023-02-11 17:10:11 -0700

    Update contacts.js
    Add validation to Contacts route

--- route/contacts.js
+++ route/contacts.js
@@ -1,5 +1,6 @@
 // Create the router object
 const route = require('express').Router();
+const validate = require('../validate/contacts');
 
 // Require Contacts controller
 const contactsController = require('../controller/contacts');
@@ -9,10 +10,26 @@ route.get('/', contactsController.getContacts);
 route.get('/:id', contactsController.getOne);
 
 // Handles any http POST requests
-route.post('/', contactsController.createContact);
+route.post('/', 
+    validate.firstName, 
+    validate.lastName,
+    validate.email,
+    validate.favoriteColor,
+    validate.birthday,
+    validate.validation, 
+    contactsController.createContact
+);
 
 // Handles any http PUT requests
-route.put('/:id', contactsController.updateContact);
+route.put('/:id', 
+    validate.firstName, 
+    validate.lastName,
+    validate.email,
+    validate.favoriteColor,
+    validate.birthday,
+    validate.validation, 
+    contactsController.updateContact
+);
 
 // Handles any http DELETE requests
 route.delete('/:id', contactsController.deleteContact);
