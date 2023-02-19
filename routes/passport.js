cf496e458f59687853f2b4288da973848094ec77
Chones17 <chones17@byui.edu>
2023-02-18 21:01:05 -0700

    Update passport.js
    Refactor route to handle passport-github2

--- routes/passport.js
+++ routes/passport.js
@@ -1,16 +1,16 @@
+// Create the router object
 const route = require('express').Router();
-// Redirect the user to the OAuth 2.0 provider for authentication.  When
-// complete, the provider will redirect the user back to the application at
-//     /auth/provider/callback
-route.get('/auth/provider', passport.authenticate('provider'));
+const passport = require('../middlewares/passport');
 
-// The OAuth 2.0 provider has redirected the user back to the application.
-// Finish the authentication process by attempting to obtain an access
-// token.  If authorization was granted, the user will be logged in.
-// Otherwise, authentication has failed.
-route.get(
-    '/auth/provider/callback', 
-    passport.authenticate('provider', { successRedirect: '/', failureRedirect: '/login' })
+// Handles and GET HTTP requests
+route.get('/', passport.authenticate('github'));
+route.get('/callback', 
+    passport.authenticate(
+        'github', { 
+            successRedirect: '/', 
+            failureRedirect: '/login' 
+        }
+    )
 );
 
 module.exports = route;
