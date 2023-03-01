b1eb400411af6cc64162369fb53e55017571b5be
Chones17 <chones17@byui.edu>
2023-02-28 23:24:33 -0700

    Update passport.js
    Refactor passport authentication routes to redirect to home and login routes

--- route/passport.js
+++ route/passport.js
@@ -3,9 +3,9 @@ const route = require('express').Router();
 const passport = require('passport');
 
 // Handles and GET HTTP requests
-route.get('/', passport.authenticate('github', { scope: [ 'user:email' ] }));
+route.get('/', passport.authenticate('github', { scope: [ 'user:email' ]}));
 route.get('/callback', passport.authenticate('github', { 
-        successRedirect: '/photos', 
-        failureRedirect: '/api-docs' }));
+        successRedirect: '/',
+        failureRedirect: '/login' }));
 
 module.exports = route;
