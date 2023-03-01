845848ae119f74479fdc58bf638d8c7d6fce9769
Chones17 <chones17@byui.edu>
2023-02-28 23:23:41 -0700

    Update index.js
    Add home, login, and logout routes

--- route/index.js
+++ route/index.js
@@ -7,5 +7,10 @@ route.use('/users', require('./user'));
 route.use('/api-docs', require('./swagger'));
 route.use('/auth/github', require('./passport'));
 
+// Handles GET requests
+route.get('/', (req, res) => res.send('Welcome to Photo API'));
+route.get('/login', (req, res) => res.send('Call ../auth/github to authenticate'));
+route.get('/logout', (req, res) => { req.logout(() => { res.redirect('/') })});
+
 // Export routes object
 module.exports = route;
